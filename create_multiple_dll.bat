c37d46c47c938a9eb954a9ade609bdb6e7953626
Rottenham <54915258+Rottenham@users.noreply.github.com>
2023-09-13 02:56:26 -0700

    run tasks in parallel

--- create_multiple_dll.bat
+++ create_multiple_dll.bat
@@ -8,9 +8,8 @@ set "subunit_num=2"
 set "PATH=%avz_dir%\MinGW\bin;%PATH%"
 
 for /L %%N in (1, 1, %subunit_num%) do (
-    "%avz_dir%\MinGW\bin\g++" -g %source% -std=c++1z -I "%avz_dir%\inc" -lavz -lgdi32 -L "%avz_dir%\bin" -shared -DP%%N -o "./bin/libavz_%%N.dll"
-    echo Finished %%N
+    start "Compilation Task (%%N/%subunit_num%)" "%avz_dir%\MinGW\bin\g++" -g %source% -std=c++1z -I "%avz_dir%\inc" -lavz -lgdi32 -L "%avz_dir%\bin" -shared -DP%%N -o "./bin/libavz_%%N.dll"
 )
 
-echo Finished.
+echo Please wait for all windows to close.
 pause
